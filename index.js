d13f0922ee93b200e19bb3a0d2434df0ab2ded85
Alessandro Sarzina <a.sarzina@palmabit.com>
2018-12-21 00:19:27 +0100

    start

--- index.js
+++ index.js
@@ -26,38 +26,10 @@ solid.auth.trackSession(session => {
 
 const start = () => {
   $('#view').click(async function loadProfile() {
-    // Load the person's data into the store
-    const person = $('#profile').val();
-    await fetcher.load(person);
-
-    // Display their details
-    const fullName = store.any($rdf.sym(person), FOAF('name'));
-    $('#fullName').text(fullName && fullName.value);
+    // TODO
   });
 
   $('#get-friends').click(async () => {
-    const person = $('#profile').val();
-    await fetcher.load(person);
-    
-    const friends = store.each($rdf.sym(person), FOAF('knows'));
-    console.log('friends', friends);
-
-    $('#friends').empty();
-
-    if (friends.length === 0) {
-      $('#friends').hide();
-      $('#friends-msg').text('No friends :-(');
-    } else {
-      $('#friends').show();
-      $('#friends-msg').html(friends.length + ' friend' + (friends.length > 1 ? 's' : '') + '!<br/>');
-    }
-
-    friends.forEach(async (friend) => {
-      console.log('friend', friend)
-      await fetcher.load(friend);
-      const fullName = store.any(friend, FOAF('name'));
-      $('#friends').append($('<li>')
-         .text(fullName && fullName.value || friend.value));
-    });
+    // TODO
   });
 }
